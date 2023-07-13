3b521263ef4b859a54a5bf7d254d36a8bea42fa2
Arno Pan <apan11@my.bcit.ca>
2023-07-13 12:58:31 -0700

    Initial Commit

---
+++ SwiftUI-TipCalculator/SwiftUI_TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TipCalculatorApp.swift
+//  SwiftUI-TipCalculator
+//
+//  Created by Arno Pan on 2023-07-13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
