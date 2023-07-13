d85df685c705436f41658a7df61c5f821fcd1886
Arno Pan <apan11@my.bcit.ca>
2023-07-13 14:47:24 -0700

    Changed keyboard to use numeric only

--- SwiftUI-TipCalculator/ContentView.swift
+++ SwiftUI-TipCalculator/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var total = "0.00"
+    @State var tipPercent = 15.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            HStack {
+                Image(systemName: "dollarsign.circle.fill")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                    .font(.title)
+                Text("Tip Calculator")
+                    .font(.title)
+                    .fontWeight(.bold)
+            }
+            HStack{
+                Text("$")
+                TextField("Amount", text: $total).keyboardType(.decimalPad)
+            }
+            HStack {
+                Slider(value: $tipPercent, in: 1...30, step: 1.0)
+                Text("\(Int(tipPercent))%")
+            }
+            if let totalNumber = Double(total){
+                VStack(alignment: .leading) {
+                    Text("Tip Amount: $\(totalNumber * tipPercent / 100, specifier: "%0.2f")")
+                    Text("Total Amount: $\(totalNumber + totalNumber * tipPercent / 100, specifier: "%0.2f")")
+                }
+            } else {
+                Text("Please enter a numeric value.")
+            }
+        }.padding()
     }
 }
 
